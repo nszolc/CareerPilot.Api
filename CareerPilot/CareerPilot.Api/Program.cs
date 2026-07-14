54c4fdbce0e86c0b143bb57ee8586cc242fa86fa
Natalia <nataliaszolc.kontakt@gmail.com>
2026-07-14 19:24:06 +0200

    feat(api): wire up DbContext, Swagger and FluentValidation
    Rejestracja AppDbContext na SQL Serverze, kontrolerow, Swaggera i walidatorow
    z FluentValidation, wraz z pakietami, ktorych ten kod wymaga. Connection string
    dla lokalnej bazy w appsettings.Development.json.

--- CareerPilot/CareerPilot.Api/Program.cs
+++ CareerPilot/CareerPilot.Api/Program.cs
@@ -1,6 +1,34 @@
+using CareerPilot.Application.Validators.JobAd;                                                                        
+using CareerPilot.Infrastructure.Data;                                                                                 
+using FluentValidation;                                                                                                
+using Microsoft.EntityFrameworkCore;                                                                                   
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>                                                                 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddValidatorsFromAssemblyContaining<CreateJobAdRequestValidator>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
