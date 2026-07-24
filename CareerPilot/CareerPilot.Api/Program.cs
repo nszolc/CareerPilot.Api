c9c84c7c35abbf085ceb0c04317efd7fee056f52
Natalia <nataliaszolc.kontakt@gmail.com>
2026-07-24 20:46:53 +0200

    feat(api): register repository and file service in DI

--- CareerPilot/CareerPilot.Api/Program.cs
+++ CareerPilot/CareerPilot.Api/Program.cs
@@ -1,7 +1,10 @@
 using CareerPilot.Application.Validators.JobAd;                                                                        
 using CareerPilot.Infrastructure.Data;                                                                                 
 using FluentValidation;                                                                                                
-using Microsoft.EntityFrameworkCore;                                                                                   
+using Microsoft.EntityFrameworkCore;   
+using CareerPilot.Application.Interfaces;
+using CareerPilot.Infrastructure.Repositories;
+using CareerPilot.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,10 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>                                                                 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// rejestracja serwisów i repo
+builder.Services.AddScoped<IJobAdRepository, JobAdRepository>();
+builder.Services.AddScoped<IApplicationFileService, ApplicationFileService>();
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateJobAdRequestValidator>();
 var app = builder.Build();
