d48715a9c45568f6a4effb081f993865a18d14af
Natalia <nataliaszolc.kontakt@gmail.com>
2026-07-24 20:46:38 +0200

    feat(application): add file upload DTO and IApplicationFileService

---
+++ CareerPilot/CareerPilot.Application/DTOs/ApplicationFile/UploadApplicationFileRequest.cs
@@ -0,0 +1,13 @@
+﻿using CareerPilot.Domain.Enums;
+
+namespace CareerPilot.Application.DTOs.ApplicationFile;
+
+public record UploadApplicationFileRequest
+{
+    public string FileName { get; init; } = string.Empty;
+    public string ContentType { get; init; } = string.Empty; //MIME type
+    public long Length {get; init;}
+    public required Stream Content { get; init; }
+    public ApplicationFileType Type { get; init; }
+    public string? Description { get; init; }
+}
---
+++ CareerPilot/CareerPilot.Application/Interfaces/IApplicationFileService.cs
@@ -0,0 +1,9 @@
+﻿using CareerPilot.Application.DTOs.ApplicationFile;
+using CareerPilot.Domain.Entities;
+
+namespace CareerPilot.Application.Interfaces;
+
+public interface IApplicationFileService
+{
+    Task<ApplicationFile> UploadAsync(UploadApplicationFileRequest request);
+}
